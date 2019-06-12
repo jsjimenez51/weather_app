4c7521d6760af8b426120c55fc0f5da499467200
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2019-06-12 06:23:34 -0700

    UPDATE: destructured city object props

--- public/src/scripts/app.js
+++ public/src/scripts/app.js
@@ -1,7 +1,34 @@
 // DOM Manipulation
-
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
 
+const updateUI = (data) => {
+
+  // const cityInfo = data.cityInfo;
+  // const weather = data.weather;
+  
+  // destructure props
+  const { cityInfo, weather } = data;
+
+  // update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityInfo.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Imperial.Value}</span>
+      <span>&deg;F</span>
+    </div>
+  `;
+
+  // remove d-none class if present to display info
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+};
+
+
+// Search Bar Functionality
 const updateCity = async (city) => {
 
   const cityInfo = await getCity(city);
@@ -21,6 +48,6 @@ cityForm.addEventListener('submit', e => {
 
   // update UI with new city data
   updateCity(city)
-  .then(data => console.log(data))
+  .then(data => updateUI(data))
   .catch(err => console.log(err));
 });
