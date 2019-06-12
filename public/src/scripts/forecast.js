5cd3d0a0a1aed3f4ec328f71ed1651c411b29551
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2019-06-11 23:31:43 -0700

    UPDATE: added get weather api call

--- public/src/scripts/forecast.js
+++ public/src/scripts/forecast.js
@@ -1,5 +1,20 @@
 const key = "npHLG3SDo4DP3wu45A3jtM4aZ4VxBk7x";
 
+// get weather information
+const getWeather = async (locID) => {
+
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${locID}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return (data[0]);
+  
+
+};
+
+// get city information
 const getCity = async (city) => {
 
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -12,6 +27,9 @@ const getCity = async (city) => {
   
 };
 
-getCity('San Francisco')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+getCity('San Francisco').then(data => {
+    return getWeather(data.Key);
+  }).then(data => {
+    console.log(data);
+  }).catch(err => console.log(err));
+
