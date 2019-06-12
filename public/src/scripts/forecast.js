6f18492c0f30e749abbbdbdede9b6ba7fef5b4cd
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2019-06-11 23:17:13 -0700

    UPDATE: added in get city location fetch function

--- public/src/scripts/forecast.js
+++ public/src/scripts/forecast.js
@@ -1,1 +1,17 @@
 const key = "npHLG3SDo4DP3wu45A3jtM4aZ4VxBk7x";
+
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return (data[0]);
+  
+};
+
+getCity('San Francisco')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
